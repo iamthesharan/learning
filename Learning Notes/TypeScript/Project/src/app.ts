c4a04b6eb37bd690aa053843290f8e876966885a
iamthesharan <iamthesharan@outlook.com>
2023-07-02 14:11:55 +0530

    Moved Typescript Folder

--- Learning Notes/TypeScript/Project/src/app.ts
+++
@@ -1,6 +0,0 @@
-import { ProjectInput } from "./components/project-input";
-import { ProjectList } from "./components/project-list";
-
-const projectInput=new ProjectInput();
-const activeProjectList=new ProjectList('active');
-const finishedProjectList=new ProjectList('finished');
